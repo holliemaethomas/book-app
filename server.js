2c01082f652261dd11a64780716a5615bcc79522
holliemaethomas <holliemaethomas85@gmail.com>
2018-08-21 18:58:35 -0700

    server.js

--- server.js
+++ server.js
@@ -1,5 +1,21 @@
 'use strict';
 
+const pg = require('pg');
+const fs = require('fs');
+const express = require('express');
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+const conString = '';
+const client = new pg.Client(conString);
+client.connect();
+client.on('error', error => {
+  console.error(error);
+});
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('./public'));
 const express = require('express');
 
 const app = express();
