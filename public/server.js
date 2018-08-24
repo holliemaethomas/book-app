3d8248f23c37ea2daaea9d66b682336021ce6956
holliemaethomas <holliemaethomas85@gmail.com>
2018-08-23 19:28:27 -0700

    test

--- public/server.js
+++ public/server.js
@@ -1,4 +1,5 @@
 'use strict';
+require('dotenv').config();
 
 const pg = require('pg');
 const fs = require('fs');
@@ -6,13 +7,25 @@ const express = require('express');
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-const conString = 'postgres://localhost:5432/lab_9';
-const client = new pg.Client(conString);
+const conString = 'postgres://localhost:5432/books_app';
+const client = new pg.Client(process.env.DATABASE_URL);
 client.connect();
 client.on('error', error => {
   console.error(error);
 });
 
+app.set('view engine', 'ejs');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('./public'));
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+app.get('/hello'), (request, response) => {
+  response.render('index');
+}
+
+app.get('books', (request, response) =>{
+response.send('client.query('SELECT DataTransferItemList, author, image_url FROM books;')
+.then(results => {response.send(results.rows);
+  response.render('index');
+});
+});
