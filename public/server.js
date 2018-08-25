218d587ef9ce0cd8b7bded7d5e301f8191998b59
holliemaethomas <holliemaethomas85@gmail.com>
2018-08-25 13:07:42 -0700

    working

--- public/server.js
+++ public/server.js
@@ -1,31 +1,46 @@
-'use strict';
+'use strict'
+
 require('dotenv').config();
 
-const pg = require('pg');
-const fs = require('fs');
+// Application Dependencies
 const express = require('express');
-const PORT = process.env.PORT;
-const app = express();
+const pg = require('pg');
 
-const conString = 'postgres://localhost:5432/books_app';
+// Application Setup
+const app = express();
+const PORT = process.env.PORT;
+
+// Database Setup
 const client = new pg.Client(process.env.DATABASE_URL);
 client.connect();
-client.on('error', error => {
-  console.error(error);
-});
+client.on('error', err => console.error(err));
 
+// Serve static files
+app.use(express.static('public'));
+
+// Set the view engine for server-side templating
 app.set('view engine', 'ejs');
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static('./public'));
-app.listen(PORT, () => console.log(`Listening on ${PORT}`));
-app.get('/hello'), (request, response) => {
-  response.render('index');
-}
 
-app.get('books', (request, response) =>{
-response.send('client.query('SELECT DataTransferItemList, author, image_url FROM books;')
-.then(results => {response.send(results.rows);
-  response.render('index');
+app.get('/ping', (request, response) => {
+  response.send('pong');
 });
+
+app.get('/hello', (request, response) => {
+  response.render('pages/hello.ejs');
 });
+
+app.get('/books', (request, response) => {
+  client.query(`
+      SELECT title, author, image_url
+      FROM books
+    `)
+    .then(result => {
+      response.render('index', { books: result.rows });
+    })
+});
+
+app.get('*', (request, response) => {
+  response.render('pages/error');
+})
+
+app.listen(PORT, () => console.log('Listening on PORT', PORT));
