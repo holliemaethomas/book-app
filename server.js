433de502a8c39f2c2be2b3295a9b4e3149b35cfc
holliemaethomas <holliemaethomas85@gmail.com>
2018-08-30 20:22:33 -0700

    update

--- server.js
+++ server.js
@@ -1,25 +1,30 @@
 'use strict'
-
+//dependencies
 require('dotenv').config();
-
-
 const express = require('express');
 const pg = require('pg');
 const app = express();
+const superagent = require('superagent');
 
+//server set up
 const PORT = process.env.PORT;
 const client = new pg.Client(process.env.DATABASE_URL);
 
-
-
+//database connect
 client.connect();
 client.on('error', err => console.error(err));
 
+//view engine connect
 app.set('view engine', 'ejs');
 
+//set as static
 app.use(express.static('./public'));
 
+//API
 
+
+
+//queries
 app.get('/books', (request, response) => {
   client.query(`
       SELECT title, author, image_url, id
@@ -43,5 +48,10 @@ app.get('/books/:id', (request, response) => {
 app.get('*', (request, response) => {
   response.render('pages/error');
 })
+
+
+
+
+//listener
 app.listen(PORT, () => console.log(`Listening on PORT`,PORT));
 
